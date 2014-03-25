f2f000a41f0465c648a006c1f5ed5e383cf97a08
Jens Balvig <jens@balvig.com>
2014-03-25 16:08:24 +0100

    simplify dummy app

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,17 +1,15 @@
 class AppDelegate < PM::Delegate
   def on_load(app, options)
     return true if App.environment == 'test'
-    @screen_1 = BaseScreen.new(nav_bar: true)
-    @screen_2 = BaseScreen.new(nav_bar: true)
-
-    @screen_1.set_tab_bar_item title: 'Balvig', system_icon: :more
-    @screen_2.set_tab_bar_item title: 'Rubymotion', system_icon: :favorites
-
-    open_tab_bar @screen_1, @screen_2
 
     BaseScreen.sync_sessions do
-      @screen_1.path = '/balvig'
-      @screen_2.path = '/rubymotion'
+      @screen_1 = BaseScreen.new(nav_bar: true, path: '/balvig')
+      @screen_2 = BaseScreen.new(nav_bar: true, path: '/rubymotion')
+
+      @screen_1.set_tab_bar_item title: 'Balvig', system_icon: :more
+      @screen_2.set_tab_bar_item title: 'Rubymotion', system_icon: :favorites
+
+      open_tab_bar @screen_1, @screen_2
     end
   end
 end
