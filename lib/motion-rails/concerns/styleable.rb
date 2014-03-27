ac3a8f0735a73f315da139273a4774c858c9d87d
Jens Balvig <jens@balvig.com>
2014-03-27 08:32:52 +0100

    allow simpler setting of tab_bar_items

--- lib/motion-rails/concerns/styleable.rb
+++ lib/motion-rails/concerns/styleable.rb
@@ -3,22 +3,18 @@ module MotionRails
 
     def on_init
       super
+      self.title = nil
       set_style_class if using_freestyle_css?
       set_webview_options
+      set_tab_bar_options
       remove_back_button_label
-      self.title = nil
+    end
+
+    def tab_bar=(tab_bar)
+      @tab_bar = tab_bar
     end
 
     private
-
-    def remove_back_button_label
-      navigationItem.backBarButtonItem = UIBarButtonItem.alloc.initWithTitle("", style: UIBarButtonItemStylePlain, target: nil, action: nil)
-    end
-
-    def set_webview_options
-      set_attributes webview, keyboard_display_requires_user_action: false, suppresses_incremental_rendering: true, background_color: UIColor.whiteColor
-      set_attributes webview.scrollView, deceleration_rate: 0.999
-    end
 
     def using_freestyle_css?
       view.respond_to?(:styleClass)
@@ -34,5 +30,19 @@ module MotionRails
       style_class += ' modal' if modal?
       style_class
     end
+
+    def set_webview_options
+      set_attributes webview, keyboard_display_requires_user_action: false, suppresses_incremental_rendering: true, background_color: UIColor.whiteColor
+      set_attributes webview.scrollView, deceleration_rate: 0.999
+    end
+
+    def set_tab_bar_options
+      set_tab_bar_item title: @tab_bar[:title], icon: Icon.new(@tab_bar[:icon], 25) if @tab_bar
+    end
+
+    def remove_back_button_label
+      navigationItem.backBarButtonItem = UIBarButtonItem.alloc.initWithTitle("", style: UIBarButtonItemStylePlain, target: nil, action: nil)
+    end
+
   end
 end
